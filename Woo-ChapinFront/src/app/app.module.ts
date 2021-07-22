1b78a8f313c0ef649414a6bc2be5cb7ba95bb6d8
Davis <davisrdln@gmail.com>
2021-07-22 01:48:19 -0600

    se agregó maquetación

--- Woo-ChapinFront/src/app/app.module.ts
+++ Woo-ChapinFront/src/app/app.module.ts
@@ -1,18 +1,44 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/global/navbar/navbar.component';
+import { FooterComponent } from './components/global/footer/footer.component';
+import { CarouselComponent } from './components/global/carousel/carousel.component';
+import { HomeComponent } from './components/general/home/home.component';
+import { LoginComponent } from './components/general/login/login.component';
+import { RegisterComponent } from './components/general/register/register.component';
+import { CategoriasComponent } from './components/general/categorias/categorias.component';
+import { CarritoComponent } from './components/general/carrito/carrito.component';
+import { InterceptorService } from './services/interceptor/interceptor.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CategoriasComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatProgressBarModule
   ],
-  providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Woo-ChapinFront/src/app/components/global/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from 'src/app/services/loader/loader.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+  show = true;
+  showFixed = false;
+  constructor(public loader: LoaderService,) { 
+    this.loader.isLoading.next(true);
+
+  }
+  
+  changeClass(){
+
+    let scrollPosition = window.pageYOffset;
+    if(scrollPosition != 0){
+      if(this.showFixed == false ){
+        this.showFixed = true;
+        this.show = false;
+      }
+    }else{
+      if(this.show == false ){
+        this.show = true;
+        this.showFixed = false;
+      }
+    }
+
+    
+  }
+  ngOnInit(): void {
+  }
+
+}
