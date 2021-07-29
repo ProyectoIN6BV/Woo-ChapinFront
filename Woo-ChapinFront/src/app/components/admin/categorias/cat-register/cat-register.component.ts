b0fce9f240ee170229825b7a020e815bbc556753
Davis <davisrdln@gmail.com>
2021-07-29 00:25:01 -0600

    se agrego funcionamiento de categorias admin

--- Woo-ChapinFront/src/app/components/admin/categorias/cat-register/cat-register.component.ts
+++ Woo-ChapinFront/src/app/components/admin/categorias/cat-register/cat-register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
+import { Categoria } from 'src/app/models/Categorias';
+import { CONNECTION } from 'src/app/services/global';
+import { RestCategoriaService } from 'src/app/services/restCategoria/rest-categoria.service';
 
 @Component({
   selector: 'app-cat-register',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatRegisterComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  categories:[];
+  private readonly notifier;
+  public uri;
+  categoria:Categoria;
+  constructor(private notifierService:NotifierService, private restCategoria: RestCategoriaService) { 
+    this.notifier = notifierService;
+    this.uri = CONNECTION.URI;
+    this.categoria = new Categoria('','','','');
   }
 
+  ngOnInit(): void {
+    this.getCategory();
+  }
+
+
+  getCategory(){
+    this.restCategoria.getCategoryAdmin().subscribe((res:any)=>{
+      if(res.categories){
+        this.categories = res.categories;
+      }else{
+        this.notifier.notify("error", res.message);
+      }
+    }, error=> this.notifier.notify("error", "ocurrió un error en la consulta"));
+  }
+
+  updateCategory(){
+    this.restCategoria.updateCategory(this.categoria, this.categoria._id).subscribe((res:any)=>{
+      if(res.updated){
+        this.notifier.notify("success", res.message);
+        this.getCategory();
+      }else{
+        this.notifier.notify("error", res.message);
+        this.getCategory();
+      }
+    }, error=> this.notifier.notify("error", "ocurrió un error en la consulta") );
+  }
+
+  removeCategory(){
+    this.restCategoria.removeCategory(this.categoria._id).subscribe((res:any)=>{
+      console.log(res);
+      if(res.categoryDeleted){
+        this.notifier.notify("success", res.message);
+        this.getCategory();
+      }else{
+        this.notifier.notify("error", res.message);
+      }
+    }, error=> this.notifier.notify("error", "ocurrió un error en la consulta"))
+  }
+
+  selectCategory(category){
+    this.categoria = category;
+  }
+
+  reset(){
+    this.getCategory();
+  }
 }
